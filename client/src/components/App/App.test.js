7e92c030d3d83313f00ec514e070f0ec064d9da8
sahil15 <sahil.atreja@newgen.co.in>
2021-01-08 15:31:52 +0000

    Updating the test case

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,11 +1,11 @@
 import { mount } from 'enzyme';
 import App from './App';
-import CharacterName from './components/Home/Home';
+import Home from './components/Home/Home';
 
-test('renders learn react link', () => {
+test('renders the App Component', () => {
   const app = mount(<App />);
 
   it("should show the HomeComponent", () => {
-    expect(wrapper.find(CharacterName).length === 1).toBeTruthy();
+    expect(wrapper.find(Home).length === 1).toBeTruthy();
   });
 });
